65bf12335d75a8c32156cfa217448fe1b933f81e
aditya.7sinha@gmail.com <aditya.7sinha@gmail.com>
2024-02-29 23:28:31 -0700

    2/29/2024

--- CodeForces/Codentine'sDay/ArcadeGame.java
+++ CodeForces/Codentine'sDay/ArcadeGame.java
@@ -1,9 +1,40 @@
-
-
+import java.util.ArrayList;
+import java.util.Scanner;
+// NEED TO FIX FOR SECOND TEST CASE EXCEEDS LIMIT
 public class ArcadeGame {
 
     public static void main(String[] args){
-        System.out.println("Hi");
+//        int arr[]= new int[3];
+//        System.out.println(arr[3]);
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int q = scanner.nextInt();
+        int[] arr = new int[n];
+        while(q-->0){
+            int c = scanner.nextInt();
+            if ( c ==1 ){
+                int l = scanner.nextInt();
+                int r = scanner.nextInt(),sum=0;
+                for(int i=l;i<=r;i++){
+                    sum+=arr[i-1];
+                }
+                System.out.println(sum);
+            }
+            else {
+                int l = scanner.nextInt();
+                int r = scanner.nextInt();
+                int p = scanner.nextInt();
+                int k = scanner.nextInt();
+                int v = scanner.nextInt();
+                for( int i=l;i<=r;i++) {
+                    if(i%p==k){
+                        arr[i-1]+=v;
+                    }
+                }
+            }
+//            System.out.println("Run "+ q+ "for");
+//            q--;
+        }
     }
 
 }
--- LeetCode/LengthOfLastWord.java
+++ LeetCode/LengthOfLastWord.java
@@ -1,2 +1,17 @@
-package LeetCode;public class LengthOfLastWord {
+package LeetCode;
+
+import java.util.Scanner;
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.split(" ");
+        String w = words[words.length -1];
+        return w.length();
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String sen = sc.nextLine();
+        LengthOfLastWord word =  new LengthOfLastWord();
+        System.out.println(word.lengthOfLastWord(sen));
+
+    }
 }
