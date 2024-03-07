6d637f7f2949648169e9bdd11d6537d2ac12a60f
aditya.7sinha@gmail.com <aditya.7sinha@gmail.com>
2024-03-07 00:05:34 -0700

    3/6/2024

--- LeetCode/ValidParenthesis.java
+++ LeetCode/ValidParenthesis.java
@@ -5,6 +5,23 @@ import java.util.Stack;
 public class ValidParenthesis {
 
     public boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if( c == '{' || c == '(' || c=='[')
+                st.push(c);
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top = st.peek();
+                if((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '['))
+                    st.pop();
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
 //        Stack<Character> stack = new Stack<Character>();
 //        for(int i=0; i<s.length();i++) {
 //            if(s.charAt(i) == '}' && stack.peek() != '{'){
