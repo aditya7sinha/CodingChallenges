04cbe33eb568a271525dcc757f0a5a815b268c64
aditya.7sinha@gmail.com <aditya.7sinha@gmail.com>
2024-03-10 22:54:03 -0700

    Tree Programs

---
+++ LeetCode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+ List<Integer> list = new ArrayList<>();
+ public List<Integer> inorderTraversal(TreeNode root) {
+  if(root==null) return new ArrayList<>();
+  inorderTraversal(root.left);
+   list.add(root.val);
+   inorderTraversal(root.right);
+  return list;
+ }
+
+}
+
